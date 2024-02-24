b4b1ebfcf5894deff8c99df0e84c4e8bafdd2f28
DELL <k.islam@clementinedatabd.com>
2024-02-24 23:47:50 +0600

    some update

--- app/src/main/java/com/example/baseproject/di/helper/HeaderModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.baseproject.di.helper
-
-data class HeaderModel(
-    val packageName: String,
-    val versionName: String,
-    val language: String,
-    val deviceId: String
-)
